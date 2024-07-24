8b7122cf8f0aab93aa28f25ae2d46ceebaa46b02
thulane <111046874+tivanethulane@users.noreply.github.com>
2024-07-24 16:25:02 +0200

    front page display for users
    This what users will see for now

---
+++ src/javascript.js
@@ -0,0 +1,11 @@
+document.getElementById("signin").addEventListener("click",function(){
+
+    window.location.href ="";
+});
+
+document.getElementById("singup").addEventListener("click",function()
+{
+  window.location.href = " ";
+});
+
+
